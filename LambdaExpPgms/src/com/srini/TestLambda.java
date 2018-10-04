a50ccb9c79f44d8df2c87c1b276eea380001cb5f
srinivasulu <srini@gmail.com>
2018-10-04 23:00:30 +0530

    fifth commit

--- LambdaExpPgms/src/com/srini/TestLambda.java
+++ LambdaExpPgms/src/com/srini/TestLambda.java
@@ -3,7 +3,7 @@ package com.srini;
 @FunctionalInterface
 interface intera
 {
-	public void m111();
+	public void m1114();
 
 	default void m2() {
 		System.out.println("Default Method of Interface....");
